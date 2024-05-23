44b8ed4e876c1eb4fb25c2f824efd794e72bab80
Xavek <theofficalgautam200@gmail.com>
2024-05-23 10:18:23 +0545

    add check stake balance

--- client/src/navbar.jsx
+++ client/src/navbar.jsx
@@ -1,11 +1,27 @@
 import { useAccount, useConnect, useDisconnect } from "@starknet-react/core";
+import { useState } from "react";
 import { sliceAddressForView } from "./lib/utils";
+import { getStakedStarkAmount } from "./lib/stakeApi";
+import { stakeManagerInstance } from "./lib/stakeManager";
+import { ethers } from "ethers";
 
 const Navbar = () => {
   const { connect, connectors } = useConnect();
   const { address, status } = useAccount();
   const { disconnect } = useDisconnect();
+  const [balance, setBalance] = useState("0");
 
+  async function fetchStakeBalance() {
+    if (status === "connected") {
+      const stakeBalance = await getStakedStarkAmount(
+        stakeManagerInstance,
+        address,
+      );
+      setBalance(ethers.formatEther(stakeBalance));
+    } else {
+      setBalance("0");
+    }
+  }
   return (
     <>
       <nav className="bg-black p-4 flex justify-between">
@@ -13,6 +29,12 @@ const Navbar = () => {
           <span className="text-white text-xl font-bold">StakeStark</span>
         </div>
         <div className="flex items-center ">
+          <button
+            className="bg-white text-black px-6 py-2 mx-2 rounded-md"
+            onClick={fetchStakeBalance}
+          >
+            Balance: {balance}
+          </button>
           <ul className="flex items-start">
             {status === "disconnected" &&
               connectors.map((connector) => (
